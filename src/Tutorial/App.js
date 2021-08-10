3053552c2c980890d301f30f2cc09c9563336e27
Shubham8968 <Shivamno1420@gmail.com>
2021-08-10 17:08:25 +0530

    this is new app.css and React Hook file

---
+++ src/Tutorial/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import UseStateErrorEx from "./tutorial/useStateErrorEx"
+function App() {
+    return (
+        <div className='container'>
+            <h2> Advance React JS </h2>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Tutorial/useStateErrorExample.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+
+function useStateErrorEx() {
+    return (
+        <div>
+            <h2> USE state Error Example </h2>            
+        </div>
+    )
+}
+ 
+export default useStateErrorEx
