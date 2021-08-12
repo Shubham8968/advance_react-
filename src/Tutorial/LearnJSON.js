6b09b39ceb2dd04072be78effda690ccd9da81b6
Shubham8968 <Shivamno1420@gmail.com>
2021-08-12 11:03:28 +0530

    This is New json file in which we Use promise function

--- src/Tutorial/LearnJSON.js
+++ src/Tutorial/LearnJSON.js
@@ -4,13 +4,27 @@ import axios from 'axios'
 function LearnJSON() {
     const handleClick = (b) =>{
         const promise = axios.post(" http://localhost:4000/users" ,{
-            name: "Shubham marzara"
+            name: "Shubham marzara"},{
+                headers: {
+                    ['content-type']: 'application/json' // setting  the server
+
+                }}
+        )
+        promise.then(response => {
+            console.log(response) // this is used to console log our response from th server 
         })
+        promise.catch(error => {
+            console.error(error); //this os used to console our error incase we get one 
+        })
+        let c=b;
+        console.log(c);
     }
     return (
         <>
-        <button type="button" className='btn' onClick={handleClick}>send User</button>
-        <button type="button" className='btn' onClick={getUsers}>get Users</button>    
+        <h2> Random Title </h2>
+        <button type="button" className='btn' onClick={handleClick}> 
+        Post User</button>
+        {/*<button type="button" className='btn' onClick={getUsers}>get Users</button> */}    
         </>
 
     )
