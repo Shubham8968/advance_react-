70e2f87aed46f5ca323f586dff4850b3ddf4ca61
Shubham8968 <Shivamno1420@gmail.com>
2021-08-13 12:57:08 +0530

    we have doing prop-drilling function in js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactRouterDom from './tutorial/React-Router';
-
+//import ReactRouterDom from './tutorial/React-Router';
+import PropDrilling from './tutorial/Prop-Drilling'
 
 function App(){
     return(
         <div className='container'>
-    
-      <ReactRouterDom />
+         <PropDrilling /> 
+      {/*<ReactRouterDom />*/}
       
         </div>
     );
