76770e1f68854b222278a3a6fb59f68f5a9fc9af
Shubham8968 <Shivamno1420@gmail.com>
2021-08-13 12:08:28 +0530

    coverup of React router Dom

--- src/Tutorial/React-Router/NavBar.js
+++ src/Tutorial/React-Router/NavBar.js
@@ -6,14 +6,14 @@ function NavBar() {
         <nav>
             <ul>
                 <li>
-                    <Link to="/" className="btn">Home</Link>
+                    <Link to="/" className="item">Home</Link>
                 </li>
                 <li>
-                    <Link to="/about" className="btn">About</Link>
+                    <Link to="/about" className="item">About</Link>
                 
                 </li>
                 <li>
-                    <Link to ="/people" className="btn">People</Link>
+                    <Link to ="/people" className="item">People</Link>
                 </li>
             </ul>
         </nav>
--- src/Tutorial/React-Router/Person.js
+++ src/Tutorial/React-Router/Person.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { data } from "../data";
+import { Link, useParams } from "react-router-dom";
 
 function Person() {
-    return (
-        <div>
-            
-        </div>
-    )
+  console.log(useParams());
+  const [name, setName] = useState("Deafult State");
+  const { id } = useParams();
+  useEffect(() => {
+    const newPerson = data.find((person) => person.id === parseInt(id));
+    setName(newPerson.name);
+  }, []);
+  return (
+    <>
+      <div>
+        <hi>{name}</hi>
+        <Link to="/people" className="btn">
+          Back to people
+        </Link>
+      </div>
+    </>
+  );
 }
 
-export default Person
+export default Person;
